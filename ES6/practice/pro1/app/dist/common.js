d24c92d430452d1fb38e4c5194e444beaf33c61c
Nastya <avy-w@yandex.ru>
2019-01-05 23:47:57 +0300

    promises

--- ES6/practice/pro1/app/dist/common.js
+++ ES6/practice/pro1/app/dist/common.js
@@ -1,6 +1,24 @@
-let f = () => {
-    return console.log('nnnnn'), 'ffffff';
-};
+'use strict';
 
-console.log(f());
-//# sourceMappingURL=common.js.map
+function test() {
+
+    return new Promise(function (resolve, reject) {
+        var input = document.getElementById('test-promise');
+
+        input.onkeyup = function () {
+            if (+input.value) resolve(input);else reject(new Error("No Valid"));
+        };
+    });
+}
+
+test().then(function (input) {
+    console.log('Valid !!');
+    return input;
+}, function (error) {
+    console.warn(error);
+    throw Error('Just error !');
+}).then(function (input) {
+    input.style.borderColor = 'green';
+}).catch(function (error) {
+    console.log(error);
+});
