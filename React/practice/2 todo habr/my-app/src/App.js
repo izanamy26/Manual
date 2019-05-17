55f26ef9f76d6534e7d4ce015ac95d476c0cd9b4
Nastya <avy-w@yandex.ru>
2019-05-17 07:37:04 +0300

    may

--- React/practice/2 todo habr/my-app/src/App.js
+++ React/practice/2 todo habr/my-app/src/App.js
@@ -11,11 +11,27 @@ class App extends React.Component {
     this.state = {
       todos: todoData 
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+      this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed;
+          }
+          return todo;
+        });
+
+        return {
+          todos: updatedTodos
+        };
+      });
   }
 
   render() {
     const todoComponent = this.state.todos.map(item =>
-      <TodoItem key={item.id} todo={item} />); 
+      <TodoItem key={item.id} todo={item} handleChange={this.handleChange} />); 
 
     return (
  
--- React/practice/2 todo habr/my-app/src/components/Check.js
+++ React/practice/2 todo habr/my-app/src/components/Check.js
@@ -5,7 +5,8 @@ import React from 'react';
 function TodoItem(props) {
     return (
        <div className="todo-item">
-        <input id={props.todo.id} type="checkbox" name="check" checked={props.todo.completed} value={props.todo.id} />
+        <input id={props.todo.id} type="checkbox" name="check" checked={props.todo.completed} value={props.todo.id}
+         onChange={() => props.handleChange(props.todo.id)} />
         <label for={props.todo.id}>{props.todo.text}</label>
       </div>
     );
